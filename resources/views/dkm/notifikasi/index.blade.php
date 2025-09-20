3227d7c39e0fb4286bff113fd5baae825f97d687
muhammad syaifulloh <skyfoxmarket@gmail.com>
2025-09-21 00:46:10 +0700

    membuat halaman notifikasi untuk admin siapa yang melakukan interaksi

---
+++ resources/views/dkm/notifikasi/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dkm') {{-- nanti kita bikin layout dasar --}}
+@section('content')
+<div class="bg-white p-6 rounded-lg shadow">
+    <h1 class="text-2xl font-bold mb-4">Notifikasi</h1>
+    <p class="text-gray-700">
+        Ini adalah halaman Notifikasi DKM. Nantinya di sini akan ada daftar notifikasi terkait kegiatan, donasi, dsb.
+    </p>
+</div>
+@endsection
