b464867dda424184f721dad9bd69096b6698c521
muhammad syaifulloh <skyfoxmarket@gmail.com>
2025-09-29 00:05:34 +0700

    update pada dashboard dapat melihat pemasukan, pengeluaran dan total keuangan pada bulan tertentu dan kategoriArtikel dapat muncul di blade index notifikasi ketika melakukan crud

--- app/Http/Controllers/Dkm/DashboardController.php
+++ app/Http/Controllers/Dkm/DashboardController.php
@@ -6,18 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Models\Kegiatan;
 use App\Models\Artikel;
 use App\Models\JadwalImam;
+use App\Models\Pemasukkan;
+use App\Models\Pengeluaran;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $jumlahKegiatan = Kegiatan::count();
-        $jumlahArtikel = Artikel::count();
-        $jumlahJadwalImam = JadwalImam::count();;
+        $jumlahArtikel  = Artikel::count();
+        $jumlahJadwalImam = JadwalImam::count();
+        
+
+        // ambil bulan & tahun saat ini
+        $bulan = Carbon::now()->month;
+        $tahun = Carbon::now()->year;
+
+        // hitung total bulan ini
+        $totalPemasukkan = Pemasukkan::whereMonth('tanggal', $bulan)
+            ->whereYear('tanggal', $tahun)
+            ->sum('total');
+
+        $totalPengeluaran = Pengeluaran::whereMonth('tanggal', $bulan)
+            ->whereYear('tanggal', $tahun)
+            ->sum('total');
+
+        $saldoAkhir = $totalPemasukkan - $totalPengeluaran;
+
         return view('dkm.dashboard', compact(
-        'jumlahKegiatan', 
-        'jumlahArtikel',
-                'jumlahJadwalImam'
-            ));
+            'jumlahKegiatan',
+            'jumlahArtikel',
+            'jumlahJadwalImam',
+            'totalPemasukkan',
+            'totalPengeluaran',
+            'saldoAkhir',
+            'bulan',
+            'tahun'
+        ));
     }
 }
