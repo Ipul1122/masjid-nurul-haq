423e5edebd00ef01ae6a39033959cac25ba07b27
muhammad syaifulloh <skyfoxmarket@gmail.com>
2025-10-12 18:23:24 +0700

    update

--- resources/views/layouts/runningText.blade.php
+++ resources/views/layouts/runningText.blade.php
@@ -1,14 +1,12 @@
-@php
+{{-- @php
     // Mengambil data running text dari database
     $runningText = \App\Models\RunningText::first();
 @endphp
 
 @if($runningText && $runningText->content)
-{{-- Running Text Container --}}
 <div class="shadow-lg sticky top-16 z-20 overflow-hidden" style="background-color: {{ $runningText->background_color }}; color: {{ $runningText->text_color }};">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
         <div class="py-3">
-            {{-- Menggunakan class animate-marquee untuk efek bergerak --}}
             <p class="whitespace-nowrap animate-marquee">
                 {{ $runningText->content }}
             </p>
@@ -16,7 +14,6 @@
     </div>
 </div>
 
-{{-- CSS untuk animasi marquee --}}
 <style>
 @keyframes marquee {
     0% { transform: translateX(100%); }
@@ -24,9 +21,8 @@
 }
 .animate-marquee {
     display: inline-block;
-    /* padding-left memastikan teks mulai dari luar layar */
     padding-left: 100%; 
     animation: marquee 30s linear infinite;
 }
 </style>
-@endif
+@endif --}}
